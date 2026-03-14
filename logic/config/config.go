9df501bc5c052be07476ac56f638fe3c76872d03
SFMand <sultanfbm12@gmail.com>
2026-03-14 22:49:27 +0300

    refactor: decouple monitoring logic and enhance configuration validation

--- logic/config/config.go
+++ logic/config/config.go
@@ -1,6 +1,13 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"log/slog"
+	"net/url"
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 const (
 	DefaultInterval = 30 * time.Second
@@ -8,15 +15,72 @@ const (
 )
 
 type Config struct {
-	Interval string   `mapstructure:"interval"`
-	Timeout  string   `mapstructure:"timeout"`
-	Targets  []Target `mapstructure:"targets"`
+	Interval time.Duration `mapstructure:"interval"`
+	Timeout  time.Duration `mapstructure:"timeout"`
+	Targets  []Target      `mapstructure:"targets"`
 }
 
 type Target struct {
-	Name     string `mapstructure:"name"`
-	URL      string `mapstructure:"url"`
-	Type     string `mapstructure:"type"`
-	Interval string `mapstructure:"interval"`
-	Timeout  string `mapstructure:"timeout"`
+	Name     string        `mapstructure:"name"`
+	URL      string        `mapstructure:"url"`
+	Type     string        `mapstructure:"type"`
+	Interval time.Duration `mapstructure:"interval"`
+	Timeout  time.Duration `mapstructure:"timeout"`
+}
+
+func (cfg *Config) validate() error {
+	if cfg.Interval <= 0 {
+		slog.Debug("invalid global interval")
+		cfg.Interval = DefaultInterval
+	}
+	if cfg.Timeout <= 0 {
+		slog.Debug("invalid global timeout")
+		cfg.Timeout = DefaultTimeout
+	}
+
+	for i := range cfg.Targets {
+		trg := &cfg.Targets[i]
+		parsed, err := url.Parse(trg.URL)
+		if err != nil {
+			return err
+		}
+		if !isValidType(trg.Type) {
+			return fmt.Errorf("unsupported type")
+		}
+		if trg.Name == "" {
+			trg.Name = parsed.Host
+		}
+		if trg.Interval <= 0 {
+			trg.Interval = cfg.Interval
+		}
+		if trg.Timeout <= 0 {
+			trg.Timeout = cfg.Timeout
+		}
+
+	}
+
+	return nil
+}
+
+func isValidType(targetType string) bool {
+	switch targetType {
+	case "http", "https", "tcp", "grpc":
+		return true
+	default:
+		return false
+	}
+}
+
+func LoadConfig() (*Config, error) {
+	var cfg Config
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.validate(); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
